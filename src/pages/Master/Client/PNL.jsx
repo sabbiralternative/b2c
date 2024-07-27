18db27719a647888696242a96aada968314a1fde
sabbirHossain <sabbirshnt@gmail.com>
2024-07-27 13:03:43 +0600

    sellted bet implemented

--- src/pages/Master/Client/PNL.jsx
+++ src/pages/Master/Client/PNL.jsx
@@ -2,13 +2,23 @@ import useDatePicker from "../../../hooks/useDatePicker";
 import { DateRangePicker } from "rsuite";
 import "rsuite/DateRangePicker/styles/index.css";
 import useGetPNL from "../../../hooks/Master/Client/useGetPNL";
+import { useState } from "react";
+import SettleBets from "../../../components/modal/Master/SettleBets";
+import handleFormatDate from "../../../utils/handleFormatDate";
 const PNL = () => {
+  const [showBetsModal, setShowBetsModal] = useState(false);
+  const [marketId, setMarketId] = useState("");
   const downlineId = localStorage.getItem("downLineId");
-  const { formattedEndDate, formattedStartDate, onChange } = useDatePicker();
+  const { formattedEndDate, formattedStartDate, onChange } =
+    useDatePicker("currentDate");
+  const { newFormattedEndDate, newFormattedStartDate } = handleFormatDate(
+    formattedStartDate,
+    formattedEndDate
+  );
   const payload = {
     downlineId,
-    fromDate: formattedStartDate,
-    toDate: formattedEndDate,
+    fromDate: newFormattedStartDate,
+    toDate: newFormattedEndDate,
   };
   const { pnl, refetchPNL } = useGetPNL(payload);
 
@@ -18,10 +28,30 @@ const PNL = () => {
     refetchPNL();
   };
 
-  
+  const defineColor = (transfer_type, pl) => {
+    if (transfer_type === "deposit") {
+      return "pl-deposit";
+    }
+    if (transfer_type === "withdraw") {
+      return "pl-withdraw";
+    }
+    if (pl > 0) {
+      return "text-green";
+    }
+    if (pl < 0) {
+      return "text-danger";
+    }
+  };
 
   return (
     <div className="container-xxl flex-grow-1 container-p-y">
+      {showBetsModal && (
+        <SettleBets
+          setShowBetsModal={setShowBetsModal}
+          marketId={marketId}
+          searchUser={downlineId}
+        />
+      )}
       <div className="col-12">
         <div className="card">
           <div className="card-body">
@@ -83,13 +113,29 @@ const PNL = () => {
             </thead>
             <tbody className="table-border-bottom-0">
               {pnl?.map((item, i) => {
+                const handleSettledBets = (statement_type, market_id) => {
+                  if (statement_type === "Betting P&L") {
+                    setMarketId(market_id);
+                    setShowBetsModal(true);
+                  }
+                };
                 return (
-                  <tr key={i}>
+                  <tr
+                    key={i}
+                    style={{
+                      cursor: `${
+                        item?.statement_type === "Betting P&L" ? "pointer" : ""
+                      }`,
+                    }}
+                    onClick={() =>
+                      handleSettledBets(item?.statement_type, item?.market_id)
+                    }
+                  >
                     <td
                       style={{ textAlign: "right" }}
-                      className={`${
-                        item?.pl > 0 ? "text-success" : "text-danger"
-                      }`}
+                      className={`
+                        ${defineColor(item?.transfer_type, item?.pl)}
+                        `}
                     >
                       <strong>{item?.pl}</strong>
                     </td>
